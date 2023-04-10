a64e3e147960639e40637ae7c62dbe8ca20d296d
Kareem Katiya <130093787+HoneyB2020@users.noreply.github.com>
2023-04-09 21:37:51 -0700

    test4
    Added recipe converter

--- script.js
+++ script.js
@@ -3,20 +3,40 @@ const menuIcon = document.querySelector('.menu-icon');
 const menuItems = document.querySelector('nav ul');
 
 menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('active');
-  menuIcon.classList.toggle('active');
-  menuItems.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+    menuItems.classList.toggle('active');
 });
 
-function ConvertRecipe()
-{
-    let Servings = document.getElementById("input").value;
-    let FlourConverted = document.getElementById("flour");
-    let Yeast = document.getElementById("yeast");
-    let Sugar = document.getElementById("sugar");
-    let Malt = document.getElementById("malt");
-    let Egg = document.getElementById("egg");
-    let Oil = document.getElementById("oil");
+
+
+function ConvertRecipe() {
+    // let Servings = document.getElementById("input").value;
+    let rangeInput = document.getElementById('RangeInput');
+    const rangeValue = document.getElementById('RangeValue');
+    rangeValue.innerText = rangeInput.value+" Servings";
+    let Servings = rangeInput.value;
+    
+    let Flour = 1000 *Servings/18;
+    document.getElementById("flour").innerText = Flour.toFixed(0);
+    let Yeast = 2 *Servings/18;
+    document.getElementById("yeast").innerText = Yeast.toFixed(0);
+    let Sugar = 40*Servings/18;
+    document.getElementById("sugar").innerText = Sugar.toFixed(0);
+    let Malt = 9*Servings/18;
+    document.getElementById("malt").innerText = Malt.toFixed(0);
+    let Egg = 2*Servings/18;
+    document.getElementById("egg").innerText = Egg.toFixed(0);
+    let Oil = 12*Servings/18;
+    document.getElementById("oil").innerText = Oil.toFixed(0);
+    let Water = 465*Servings/18;
+    document.getElementById("water").innerText = Water.toFixed(0);
 
     
+    
+    
+
+
+
+
 }
