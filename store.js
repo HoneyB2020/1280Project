d79489eec6c3141b60ec562a775e6f6ab171935f
Kareem Katiya <130093787+HoneyB2020@users.noreply.github.com>
2023-04-12 12:31:47 -0700

    Update store.js

--- store.js
+++ store.js
@@ -6,7 +6,10 @@ if (document.readyState == 'loading') {
     ready()
 }
 
+//Event listener to remove items from cart
 function ready() {
+
+    /*Log clicks of buttons*/
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
     for (var i = 0; i < removeCartItemButtons.length; i++) {
         var button = removeCartItemButtons[i]
@@ -37,6 +40,7 @@ function purchaseClicked() {
     updateCartTotal()
 }
 
+//Update cart after clicking
 function removeCartItem(event) {
     var buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
@@ -45,6 +49,8 @@ function removeCartItem(event) {
 
 function quantityChanged(event) {
     var input = event.target
+
+    //Condition check to see if valid number or above zero
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
@@ -60,7 +66,7 @@ function addToCartClicked(event) {
     addItemToCart(title, price, imageSrc)
     updateCartTotal()
 }
-
+///cart object creation with TItle, price and imagesource that shows.
 function addItemToCart(title, price, imageSrc) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
@@ -72,6 +78,8 @@ function addItemToCart(title, price, imageSrc) {
             return
         }
     }
+
+    //create a new div/row for cart
     var cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
@@ -89,6 +97,10 @@ function addItemToCart(title, price, imageSrc) {
 }
 
 function updateCartTotal() {
+
+        //get cart row/items
+
+    ///this will get an array of items - select the first one
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
     var total = 0
@@ -96,116 +108,24 @@ function updateCartTotal() {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        //parse price and remove dollar sign
         var price = parseFloat(priceElement.innerText.replace('$', ''))
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
+    //update price total after loop
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
 
 
-// //make sure page is loaded before JS runs, otherwise it will calculate nothing
-// if (document.readyState == 'loading') {
-//     document.addEventListener('DOMContentLoaded', ready)
-// }
-// else {
-//     ready();
-// }
-
-// //Event listener to remove items from cart
-// function ready() {
-//     var removeCartItemsButton = document.getElementsByClassName('btn-danger');
-//     /*Log clicks of buttons*/
-//     for (var i = 0; i < removeCartItemsButton.length; i++) {
-//         var btn = removeCartItemsButton[i];
-//         btn.addEventListener('click', removerCartItems)
-//     }
-// }
-// /*/*/
-// var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-// for (var i = 0; i < quantityInputs.length; i++) {
-//     var input = quantityInputs[i];
-//     input.addEventListener('change', quantityChanged)
-// }
-
-// var addToCartButtons = document.getElementsByClassName('shop-item-button');
-// for (var i = 0; i < addToCartButtons.length; i++) {
-//     var button = addToCartButtons[i];
-//     button.addEventListener('clicked', addtoCartClicked)
-// }
 
 
 
-// function removerCartItems(event) {
-//     var btnClicked = event.target;
-//     btnClicked.parentElement.parentElement.remove();
-//     //Update cart after clicking
-//     UpdateTotal();
-// }
-
-// function quantityChanged(event) {
-//     var input = event.target;
-//     //is this is a number and is this below zero
-//     if (isNaN(input.value) || input.value <= 0) {
-//         input.value = 1
-//     }
-//     UpdateTotal();
-// }
-
-// function addtoCartClicked(event) {
-//     var button = event.target;
-//     var shopItem = button.parentElement.parentElement;
-//     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
-//     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
-//     var imageSRC = shopItem.getElementsByClassName('shop-item-image')[0].src;
-
-//     addItemToCart(title, price, imageSRC);
-// }
 
 
-// //create a new div/row for cart
-// function addItemToCart(title, price, imageSRC) {
-
-//     var cartRow = document.createElement('div');
-//     var cartItems = document.getElementsByClassName('cart-items')[0];
-//     var cartRowContents = ` <div class="cart-item cart-column">
-//                             <img class="cart-item-image" src="Images/Shirt.png" width="100" height="100">
-//                             <span class="cart-item-title">T-Shirt</span>
-//                             </div>
-//                             <span class="cart-price cart-column">$19.99</span>
-//                             <div class="cart-quantity cart-column">
-//                             <input class="cart-quantity-input" type="number" value="1">
-//                             <button class="btn btn-danger" type="button">REMOVE</button>
-//                             </div>`
-//     cartRow.innerHTML = cartRowContents;
-//     cartItems.append(cartRow);
-
-// }
-
-// function UpdateTotal() {
-//     //get cart row/items
-
-//     ///this will get an array of items - select the first one
-//     var cartItemContainer = document.getElementsByClassName('cart-items')[0];
-//     var cartRows = cartItemContainer.getElementsByClassName('cart-row');
-
-//     var total = 0;
-
-//     for (var i = 0; i < cartRows.length; i++) {
-//         var cartRow = cartRows[i];
-//         var priceTag = cartRow.getElementsByClassName('cart-price')[0];
-//         var quantityNumber = cartRow.getElementsByClassName('cart-quantity-input')[0];
-//         //parse price and remove dollar sign
-//         var price = parseFloat(priceTag.innerText.replace('$', ""));
-//         //
-//         var quantity = quantityNumber.value;
-
-//         total = total + (price * quantity);
 
 
-//     }
-//     //update price total after loop
-//     document.getElementsByClassName('cart-total-price')[0].innerText = total.toFixed(2) + "$";
 
-// }
+
+
