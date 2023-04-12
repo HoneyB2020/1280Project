74c83496184a48123c1c0b6eb2ef4ec956b98c0a
Kareem Katiya <130093787+HoneyB2020@users.noreply.github.com>
2023-04-11 22:16:32 -0700

    version8

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menuIcon = document.querySelector('.menu-icon');
-const menuItems = document.querySelector('nav ul');
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active');
-    menuIcon.classList.toggle('active');
-    menuItems.classList.toggle('active');
-});
-
-
-
 function ConvertRecipe() {
     // let Servings = document.getElementById("input").value;
     let rangeInput = document.getElementById('RangeInput');
@@ -18,25 +6,19 @@ function ConvertRecipe() {
     let Servings = rangeInput.value;
     
     let Flour = 1000 *Servings/18;
-    document.getElementById("flour").innerText = Flour.toFixed(0);
+    document.getElementById("flour").innerText = "Flour " + Flour.toFixed(0)+ " grams";
     let Yeast = 2 *Servings/18;
-    document.getElementById("yeast").innerText = Yeast.toFixed(0);
+    document.getElementById("yeast").innerText = "Yeast " + Yeast.toFixed(0)+ " grams";
     let Sugar = 40*Servings/18;
-    document.getElementById("sugar").innerText = Sugar.toFixed(0);
+    document.getElementById("sugar").innerText = "Sugar " + Sugar.toFixed(0)+ " grams";
     let Malt = 9*Servings/18;
-    document.getElementById("malt").innerText = Malt.toFixed(0);
+    document.getElementById("malt").innerText = "Malt Powder " + Malt.toFixed(0)+ " grams";
     let Egg = 2*Servings/18;
-    document.getElementById("egg").innerText = Egg.toFixed(0);
+    document.getElementById("egg").innerText = Egg.toFixed(0)+ " Eggs";
     let Oil = 12*Servings/18;
-    document.getElementById("oil").innerText = Oil.toFixed(0);
+    document.getElementById("oil").innerText = "Vegetable Oil "+Oil.toFixed(0)+ " grams";
     let Water = 465*Servings/18;
-    document.getElementById("water").innerText = Water.toFixed(0);
+    document.getElementById("water").innerText = "Water " +Water.toFixed(0)+ " grams";
 
     
-    
-    
-
-
-
-
 }
